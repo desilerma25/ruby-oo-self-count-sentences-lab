171d093e0ee755467cf29f879c3d8657e5d8af10
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-03 12:27:52 -0600

    commit tree 8990854cef562e567a77ff678769a03fce234480

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else 
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    # use self
+    # split string on all !?., count elements
+    self.split(/[.?!]/).reject {|repeat| repeat.size <2}.count
+    # splits on .?! and rejects if it ends with a repeated .?! then counts the elements 
+    # return number of sentences in a string
+    # return 0 if there are no sentences in a string
+    # returns the number of sentences in a complex string
   end
+
 end
